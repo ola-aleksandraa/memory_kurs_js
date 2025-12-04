1f280ed2e716df06afaa9f139a7071f28e8e8bc7
Aleksandra Siklucka <asiklucka@student.agh.edu.pl>
2025-12-04 14:43:07 +0100

    struktura plików do gry

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-import ScoreDisplay from './components/ScoreDisplay';
-import ActionButton from './components/ActionButton';
-import Player from './components/Players';
 
 //trzyma wyniki w pamięci
 //<number> oznacza e w tym use =State będzie uyta liczba
 function App() {
-  const[playerA, setPlayerA] = useState<number>(0);
-  const[playerB, setPlayerB] = useState<number>(0);
-  
+
   return (
     <>
-      <h1>Gra dla dwóch graczy</h1>
-      <Player name='Gracz A' score={playerA} onAddPoint={() => setPlayerA(playerA+1)} />
-      <Player name='Gracz B' score={playerB} onAddPoint={() => setPlayerB(playerB+1)} />
-      <button onClick = {() => {setPlayerA(0); setPlayerB(0);}}> Reset obu graczy</button>
+     
     </>
   )
 }
