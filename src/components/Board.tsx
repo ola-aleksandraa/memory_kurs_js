9f3d87b0a1f9f87c3704506ca38c6f0919fdad90
Aleksandra Siklucka <asiklucka@student.agh.edu.pl>
2025-12-14 17:19:02 +0100

    funkcja tasowania kart

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -2,14 +2,28 @@ import Card from "./Card";
 import './../styles/Board.css'
 import { easyBoard, mediumBoard, hardBoard  } from "../assets/board";
 
+function shuffleArray(array: string[]){
+
+    const newArray = []; //nowa tablica, w której będziemy umieszczać potasowane karty
+    const copy = [...array]; //kopiujemy oryginalna tabllicę, ... kopiuja tablicę bo inaczej obie by wskazywały ta sama tablicę
+
+    while (copy.length>0){
+        const index = Math.floor(Math.random() * copy.length); //Math.random wybiera losową liczbę z przedziału od 0 do 1, skalujemy to na długośc tablicy, a nstępnie Math.floor przyblia to do najblisza liczby calkowitej 
+        newArray.push(copy[index]); //dodajemy element do nowej tabllicy
+        copy.splice(index, 1); //usuwamy wylosowany element i zmniejszamy tablice o jeden oraz przechodzimy na początek pętli
+    }
+
+    return newArray;
+}
+
 function Board() {
 
+    const shuffCards = shuffleArray(mediumBoard); //wywołanie funkcji tasującej i przypisaie wyniku do stałej
     
     return (
         <div className="board">
-            {/* <Card value={123} />  value musi byc strigiem */}
-            {mediumBoard.map((card, index) => (
-                <Card key={index} value={card} />  //key to unikalny identyfikator kadego elementu, w tym wypadku uzywamy indeksu w tablicy, value czyli wartośc elementu tablicy 
+            {shuffCards.map((card, index) => (
+                <Card key={index} value={card} />  
             ))}                                     
         </div>
     );
