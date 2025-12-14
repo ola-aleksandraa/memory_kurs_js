80e09bf0b0da3c8281e5a498f048a52da3d34217
Aleksandra Siklucka <asiklucka@student.agh.edu.pl>
2025-12-14 16:26:10 +0100

    wywołanie dynamiczne

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,21 +1,32 @@
 import Card from "./Card";
 import './../styles/Board.css'
+import { easyBoard, mediumBoard, hardBoard  } from "../assets/board";
 
 function Board() {
 
-    return(
+    const cards = ['🐶', '🐱', '🐭', '🐹', '🐰', '🦊'];
+    
+    return (
         <div className="board">
-            <h2>Plansza</h2>
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+            {cards.map((card, index) => (
+                <Card key={index} value={card} />   //key to unikalny identyfikator kadego elementu, w tym wypadku uzywamy indeksu w tablicy, value czyli wartośc elementu tablicy 
+            ))}                                     
         </div>
     );
+
+/* WYNIK MAPY
+<div className="board">
+    <Card key={0} value={'🐶'} />
+    <Card key={1} value={'🐱'} />
+    <Card key={2} value={'🐭'} />
+    <Card key={3} value={'🐹'} />
+    <Card key={4} value={'🐰'} />
+    <Card key={5} value={'🦊'} />
+</div>
+*/
+
 }
+
+
 
 export default Board;
