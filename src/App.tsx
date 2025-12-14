c0f36fda058adf871a9d3c181a5fbedb8d6656a0
Aleksandra Siklucka <asiklucka@student.agh.edu.pl>
2025-12-14 22:37:01 +0100

    zadanie domowe 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,66 @@
 import './App.css'
 import Board from './components/Board'
+import { useState, type ChangeEvent } from 'react';
+import { easyBoard, mediumBoard, hardBoard  } from "./assets/board";
+
+function shuffleArray(array: string[]){
+
+    const newArray = []; //nowa tablica, w której będziemy umieszczać potasowane karty
+    const copy = [...array]; //kopiujemy oryginalna tabllicę, ... kopiuja tablicę bo inaczej obie by wskazywały ta sama tablicę
+
+    while (copy.length>0){
+        const index = Math.floor(Math.random() * copy.length); //Math.random wybiera losową liczbę z przedziału od 0 do 1, skalujemy to na długośc tablicy, a nstępnie Math.floor przyblia to do najblisza liczby calkowitej 
+        newArray.push(copy[index]); //dodajemy element do nowej tabllicy
+        copy.splice(index, 1); //usuwamy wylosowany element i zmniejszamy tablice o jeden oraz przechodzimy na początek pętli
+    }
+
+    return newArray;
+}
 
 function App() {
+
+  //useState pozwala nam zapamiętać wybrany poziom gry (stan komponentu)
+  //mamy 3 moliwe poziomy, wyjściowo ustawiamy łatwy
+  const[level, setLevel] = useState< "easy" | "medium" | "hard" >('easy');
+
+  //handleChange to funkcja, która obsługuje zmianę w <select>
+  //e to jest to co my klikamy - element select
+  //e.target.value to wybrana wartośc z <select>
+  const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setLevel(e.target.value as "easy"|"medium"|"hard");
+    setCards(getBoard(e.target.value as "easy"|"medium"|"hard"));
+  };
+
+  //wybiermy tablice z listy i zapisujemy do zmiennej currentBoard, która potem na tej podstawie zapsiuje jaką plansze ma uzyć
+  const getBoard = (level: "easy"|"medium"|"hard") => {
+    let currentBoard;
+    if (level === "easy") currentBoard = easyBoard;
+    else if (level === "medium") currentBoard = mediumBoard;
+    else currentBoard = hardBoard;
+
+    return shuffleArray(currentBoard);
+  }
+
+  //cards przechpwuje aktualna tablicę kart na planszy, domyslnie jest to tablica dla poziomu level 
+  //setCards pozwala zmienić tablicę kart na planszy 
+  const[cards, setCards] = useState<string[]>(getBoard(level));
 
   return (
     <div className='app'>
       <h1>Gra Memory</h1>
-      <Board />
+      <div className='controls'> {/*"pojemnik" z wyborem poziomu trudności*/}
+      <label>
+        Wybierz poziom trudności:
+        <select value={level} onChange={handleChange}>
+          <option value="easy">Łatwy</option>
+          <option value="medium">Średni</option>
+          <option value="hard">Trudny</option>
+        </select>
+      </label>
+      </div>
+      <Board cards={cards} level={level}/>
     </div>
-  )
+  );
 }
 
 export default App
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,28 +1,19 @@
 import Card from "./Card";
 import './../styles/Board.css'
-import { easyBoard, mediumBoard, hardBoard  } from "../assets/board";
 
-function shuffleArray(array: string[]){
 
-    const newArray = []; //nowa tablica, w której będziemy umieszczać potasowane karty
-    const copy = [...array]; //kopiujemy oryginalna tabllicę, ... kopiuja tablicę bo inaczej obie by wskazywały ta sama tablicę
-
-    while (copy.length>0){
-        const index = Math.floor(Math.random() * copy.length); //Math.random wybiera losową liczbę z przedziału od 0 do 1, skalujemy to na długośc tablicy, a nstępnie Math.floor przyblia to do najblisza liczby calkowitej 
-        newArray.push(copy[index]); //dodajemy element do nowej tabllicy
-        copy.splice(index, 1); //usuwamy wylosowany element i zmniejszamy tablice o jeden oraz przechodzimy na początek pętli
-    }
-
-    return newArray;
+interface BoardProps{
+    cards: string[];
+    level: "easy" | "medium" | "hard";
 }
 
-function Board() {
+function Board({cards, level} : BoardProps) {
 
-    const shuffCards = shuffleArray(mediumBoard); //wywołanie funkcji tasującej i przypisaie wyniku do stałej
+    //usuwamy wywołanie funkcji tasującej ze stałym zaznaczeniem poziomu poniewa będziemy wybierac poziom
     
     return (
-        <div className="board">
-            {shuffCards.map((card, index) => (
+        <div className={`board ${level}`}>
+            {cards.map((card, index) => (
                 <Card key={index} value={card} />  
             ))}                                     
         </div>
